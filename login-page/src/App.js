5851df321e856d15bb4427c49a7452e3d0d5794a
imtiyaz6320 <imtiyaz6320@gmail.com>
2022-04-30 01:52:56 +0530

    .

---
+++ login-page/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginUi from './Login Ui/Login Ui'
+
+function App() {
+  return (
+    <div className="App">
+      <LoginUi />
+    </div>
+  );
+}
+
+export default App;
